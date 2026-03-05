3f0addf5941b4afebf425e07cbe835ec6cb43e7b
Ian Marques <ian.romancini.marques@gmail.com>
2026-03-04 21:44:01 -0300

    Retry commit

--- auth_routes.py
+++ auth_routes.py
@@ -2,3 +2,8 @@ from fastapi import APIRouter
 
 auth_router = APIRouter(prefix='/auth', tags=['auth'])
 
+@auth_router.get('/')
+async def auth():
+    """Our system's authentication endpoint."""
+    return {'message': 'Rota de autenticação'}
+
