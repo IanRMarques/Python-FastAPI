a83b09aeeac71d9c237ddd2f5aa6e4fcc7929701
Ian Marques <ian.romancini.marques@gmail.com>
2026-03-07 09:41:19 -0300

    Create dependencies

--- dependencies.py
+++ dependencies.py
@@ -1,7 +1,11 @@
 from models import db
 from sqlalchemy.orm import sessionmaker #importando a classe sessionmaker do SQLAlchemy para criar sessões de banco de dados
-def pegar_sessao():
-    session = sessionmaker(bind=db) #criando uma classe de sessão vinculada ao engine do banco de dados
-    session = session() #criando uma instância de sessão para interagir com o banco de dados
 
-    return session
+def pegar_sessao():
+    try:
+        session = sessionmaker(bind=db) #criando uma classe de sessão vinculada ao engine do banco de dados
+        session = session() #criando uma instância de sessão para interagir com o banco de dados
+        yield session #utilizando yield para criar um gerador que retorna a sessão, permitindo que o FastAPI gerencie o ciclo de vida da sessão de forma eficiente
+        session.close() #fechando a sessão após o uso 
+    finally: #garantindo que aconteça caso de erro, sem tratamento de erros
+         session.close()
