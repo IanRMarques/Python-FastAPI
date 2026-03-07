a510545a8347d86e7662e79ad033c320abb6c4cc
Ian Marques <ian.romancini.marques@gmail.com>
2026-03-07 10:18:39 -0300

    Hashing passwords.

--- auth_routes.py
+++ auth_routes.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter, Depends
 from models import Usuario, db
 from dependencies import pegar_sessao
-
+from main import bcrypt_context
 auth_router = APIRouter(prefix='/auth', tags=['auth'])
 
 @auth_router.get('/')
 async def home():
     """Our system's authentication endpoint."""
     return {'message': 'Rota de autenticação', "autenticação":False}
+
+
 #criar rota para criar usuario.
 @auth_router.post('/create_user')
 #criando a função para criar um usuario, recebendo email e senha como parâmetros, e utilizando a classe Usuario do models para criar um novo usuário no banco de dados
@@ -17,7 +19,8 @@ async def create_user(email: str,senha: str, nome:str, session = Depends(pegar_s
     if usuario:
         return {'message': 'Email já cadastrado'}
     else:
-        novo_usuario = Usuario(nome, email, senha)
+        senha_criptografada = bcrypt_context.hash(senha) #criptografando a senha fornecida usando o objeto bcrypt_context configurado para usar o algoritmo bcrypt
+        novo_usuario = Usuario(nome, email, senha_criptografada) #criando uma nova instância da classe Usuario com o nome, email e senha criptografada fornecidos
         session.add(novo_usuario) #adicionando o novo usuário à sessão
         session.commit() #confirmando a transação para salvar o novo usuário no banco de dados
         return {'message': 'Usuário criado com sucesso', 'nome': nome, 'email': email}
--- main.py
+++ main.py
@@ -7,12 +7,25 @@
 #usar o alembic para criar as tabelas, mudar o arquivo alembic.ini para apontar para o banco de dados correto, depois rodar os seguintes comandos:
 
 
-
-from fastapi import FastAPI
-app = FastAPI()
 from passlib.context import CryptContext #importando a classe CryptContext do módulo passlib.context para lidar com a criptografia de senhas
+from dotenv import load_dotenv
+import os
+from fastapi import FastAPI
+load_dotenv() #carregando as variáveis de ambiente do arquivo .env para o ambiente de execução do aplicativo, permitindo o acesso a essas variáveis através do código
+    
+
+SECRET_KEY= os.getenv("SECRET_KEY") #obtendo o valor da variável de ambiente SECRET_KEY, que é usada para criptografar as senhas dos usuários, garantindo que a chave secreta seja mantida segura e não exposta no código-fonte
+
+app = FastAPI()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+ #criando um objeto CryptContext configurado para usar o algoritmo de hash bcrypt, que é uma escolha segura para armazenar senhas, 
+ #e definindo a opção deprecated como "auto" para lidar automaticamente com senhas antigas que possam ter sido hashadas com algoritmos menos seguros
 from auth_routes import auth_router
 from order_routes import order_router
+
+
+
 
 app.include_router(auth_router)
 app.include_router(order_router)
