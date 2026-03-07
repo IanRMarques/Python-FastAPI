9181820d1ebf36a65c98ece691252862ebfc1e99
Ian Marques <ian.romancini.marques@gmail.com>
2026-03-07 09:45:36 -0300

    Update gitignore

--- main.py
+++ main.py
@@ -6,9 +6,11 @@
 #..\.venv\Scripts\activate
 #usar o alembic para criar as tabelas, mudar o arquivo alembic.ini para apontar para o banco de dados correto, depois rodar os seguintes comandos:
 
+
+
 from fastapi import FastAPI
 app = FastAPI()
-
+from passlib.context import CryptContext #importando a classe CryptContext do módulo passlib.context para lidar com a criptografia de senhas
 from auth_routes import auth_router
 from order_routes import order_router
 
